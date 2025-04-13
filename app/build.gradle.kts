9b125f4e78334cab3d9a78f5bfc9ff985a8bdfc0
Kevin Escobedo <kevinescobedo248@gmail.com>
2025-04-13 19:19:10 -0300

    Commit inicial - proyecto curso kotlin desde segunda clase, configurando un tema desde material theme builder

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.compose.compiler)
     //alias(libs.plugins.dagger.hilt)
     //alias(libs.plugins.room)
     //alias(libs.plugins.ksp)
@@ -52,10 +53,6 @@ android {
         compose = true
     }
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
-    }
-
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -74,6 +71,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.ui.text.google.fonts)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
